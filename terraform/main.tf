0b77d83d60d1b6440a983c25890e34d68563f89b
Luiz Gustavo <luiz-gustavo.matias@electrolux.com>
2026-02-23 14:17:03 -0300

    feat: add S3 backend configuration for Terraform state management

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket = "ta-liso-terraform-state"
+    key    = "lambda/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
